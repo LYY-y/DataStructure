a9608491103d68a09b47e9ac9c10cf014f9598e0
LYY-y <1312152518@qq.com>
2020-01-26 20:34:03 +0800

    1-1update

--- src/Complex.java
+++ src/Complex.java
@@ -10,6 +10,16 @@ public class Complex {
         this.imag = imag;
     }
 
+    public Complex(String complexStr){
+        int plusPosition=complexStr.indexOf("+");
+        int iPosition=complexStr.indexOf("i");
+        String realStr=complexStr.substring(0,plusPosition);
+        String imagStr=complexStr.substring(plusPosition,iPosition);
+        if(iPosition==complexStr.length()-1 && realStr)
+        this.real=Integer.parseInt(realStr);
+        this.imag=Integer.parseInt(imagStr);
+    }
+
     public double getReal() {
         return real;
     }
@@ -40,9 +50,16 @@ public class Complex {
         return new Complex(complex1.real+complex2.real,complex1.imag+complex2.imag);
     }
 
-    public void minus(Complex complex){
+    public void sub(Complex complex){
         this.real-=complex.real;
         this.imag=complex.imag;
 
     }
+
+    public static Complex sub(Complex complex1,Complex complex2){
+        return new Complex(complex1.real-complex2.real,complex1.imag-complex2.imag);
+    }
+
+
+
 }
