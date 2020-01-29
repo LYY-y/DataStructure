1a073793609f6a88a1d8896e0f569dc9e14afdd3
LYY-y <1312152518@qq.com>
2020-01-29 19:45:36 +0800

    线性表Update200129

--- src/SeqList.java
+++ src/SeqList.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 //顺序表
 public class SeqList<T> extends Object {
     /** 对象数组存储顺序表的数据元素，保护成员 */
@@ -27,43 +29,92 @@ public class SeqList<T> extends Object {
 
     /**判断线性表是否为空，若空返回true*/
     public boolean isEmpty(){
-
+        return this.n==0;
     }
 
     /**返回线性表个数（长度）*/
     public int size(){
-
+        return this.n;
     }
 
     /**返回第i个元素*/
     T get(int i){
-
+        if (i>=0 && i<=this.n){
+            return (T)this.element[i];
+        }
+        return null;
     }
 
     /**设置第i个元素为x*/
     public void set(int i, T x){
-
+        if (x==null){
+            throw new NullPointerException("x=null");
+        }
+        if (i>=0 && i<this.n){
+            this.element[i]=x;
+        }else {
+            throw new java.lang.IndexOutOfBoundsException(i+"");
+        }
     }
 
     /**返回线性表所有元素的描述字符串*/
     @Override
     public String toString(){
+        String string=this.getClass().getName()+"（";
+        if (this.n>0) {
+            string += this.element[0].toString();
+            for (int i = 1; i < this.n; i++) {
+                string += "，" + this.element[i].toString();
+            }
+        }
+        return string+"）";
+    }
+
+    /**返回所有元素的描述字符串（次序从后向前），方法体省略*/
+    public String toPreviousString(){
+        String string=this.getClass().getName()+"（";
+        if (this.n>0) {
+            string += this.element[this.n - 1].toString();
+            for (int i = this.n - 2; i >= 0; i--) {
+                string += "，" + this.element[i].toString();
+            }
+        }
+        return string+"）";
 
     }
 
     /**插入x作为第i个元素，x!=null，返回x序号*/
     public int insert(int i, T x){
-
+        if (this.n==this.element.length||i>this.element.length){
+            Object[] elementTemp=this.element;
+            this.element=new Object[this.n*2];
+            for (int j=0; j<this.n; j++){
+                this.element[j]=elementTemp[j];
+            }
+        }
+        if (i>=0 && i<=this.n){
+            for (int k=this.n-1; k>=i; k++){
+                this.element[k+1]=this.element[k];
+            }
+            this.element[i-1]=x;
+        }else {
+            this.element[i-1]=x;
+        }
+        if (x != null){
+            return i-1;
+        }else {
+            return -1;
+        }
     }
 
     /**在线性表最后插入x元素，返回x序号*/
     public int insert(T x){
-
+        return this.insert(this.n,x);
     }
 
     /**删除第i个元素，返回被删除元素*/
     public T romove(int i){
-
+        
     }
 
     /**删除线性表所有元素*/
@@ -87,5 +138,18 @@ public class SeqList<T> extends Object {
     }
 
     /**删除首次出现的与key相等元素，返回被删除元素*/
-    public 
+    public T remove(T key){
+
+    }
+
+    /**比较两个线性表所有元素是否对应相等*/
+    @Override
+    public boolean equals(Object object){
+
+    }
+
+    /**在this中添加list的所有元素，集合并运算*/
+    public void addAll(List<T> list){
+
+    }
 }
