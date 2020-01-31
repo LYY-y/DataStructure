896ae956232be1bdf41b63d5bd3d00750fb4ffc8
LYY-y <1312152518@qq.com>
2020-01-31 20:58:54 +0800

    顺序表update200131

--- tests/SeqListTest.java
+++ tests/SeqListTest.java
@@ -1,5 +1,7 @@
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
+import org.omg.PortableInterceptor.INACTIVE;
 
 import static org.junit.Assert.*;
 
@@ -25,47 +27,63 @@ public class SeqListTest {
 
     @Test
     public void get() {
-        assertEquals("索引为2的值为：", 60, java.util.Optional.ofNullable(seqList.get(2)));
+        assertEquals("索引为2的值为：", 60, (Object)seqList.get(2));
     }
 
     @Test
     public void set() {
+        seqList.set(0,70);
+        System.out.print(seqList.toString());
     }
-
+@Ignore
     @Test
     public void toString1() {
-    }
 
+    }
+@Ignore
     @Test
     public void toPreviousString() {
     }
 
     @Test
     public void insert() {
+        seqList.insert(2,99);
+        Integer[] expects={5,80,99,60,90,20};
+        assertArrayEquals(expects,seqList.element);
     }
 
     @Test
     public void insert1() {
+        seqList.insert(99);
+        Integer[] expects={5,80,60,90,20,99};
+        assertArrayEquals(expects,seqList.element);
     }
 
     @Test
     public void remove() {
+        assertEquals(80,(Object)seqList.remove(1));
     }
 
     @Test
     public void clear() {
+        seqList.clear();
+        Integer[] expects={};
+        assertArrayEquals(expects,seqList.element);
     }
 
     @Test
     public void search() {
+        assertEquals(2,seqList.search(60));
     }
 
     @Test
     public void contains() {
+        assertTrue("顺序表中存在60",seqList.contains(60));
     }
 
     @Test
     public void insertDifferent() {
+
     }
 
     @Test
