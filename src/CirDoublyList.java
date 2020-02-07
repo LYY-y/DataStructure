971ffeac1c56f9b3164ecd476ddb86d908dfb082
LYY-y <1312152518@qq.com>
2020-02-07 22:34:42 +0800

    循环双链表补充200207

--- src/CirDoublyList.java
+++ src/CirDoublyList.java
@@ -1,3 +1,5 @@
+import com.sun.org.apache.bcel.internal.generic.IF_ACMPEQ;
+
 /**循环双链表*/
 public class CirDoublyList<T> {
     /**头指针*/
@@ -12,30 +14,75 @@ public class CirDoublyList<T> {
     /**重载构造方法*/
     public CirDoublyList(T[] values){
         this();
-        for (T t:values){
-            this.insert(t);
-        }
+       DoubleNode<T> p=this.head;
+       for (int i=0; i<values.length; i++){
+           p.next=new DoubleNode<T>(values[i],p,this.head);
+           p=p.next;
+       }
+       this.head.prev=p;
     }
 
     /**重载构造方法，深拷贝*/
     public CirDoublyList(CirDoublyList<T> list){
         this();
-        DoubleNode<T> p=this.head.next;
-        p.next.prev=p;
-        p.prev.next=p;
+        DoubleNode<T> p=this.head;
         DoubleNode<T> listNode=list.head.next;
+//        while (listNode.next!=list.head){
+//            p.next=new DoubleNode<T>(listNode.data,p,p.next.next);
+//            p=p.next;
+//            listNode=listNode.next;
+//        }
+//        p.next=new DoubleNode<T>(listNode.data,p,this.head);
+//        this.head.prev=p.next;
+//以下更优
         while (listNode!=list.head){
-            p=new DoubleNode<T>(listNode.data,null,null);
+            p.next=new DoubleNode<T>(listNode.data,p,this.head);
             p=p.next;
-            p.next.prev=p;
-            p.prev.next=p;
             listNode=listNode.next;
         }
+        this.head.prev=p;
     }
 
     /**判断循环双链表是否为空*/
     public boolean isEmpty(){
         return this.head.next==this.head;
+    }
+
+    public int size(){
+        DoubleNode<T> p=this.head.next;
+        int count;
+        for (count=0; p!=this.head; count++){
+            p=p.next;
+        }
+        return count;
+    }
+
+    public T get(int i){
+        DoubleNode<T> p=this.head;
+        for (int k=0; p.next!=this.head && k<=i; k++){
+            p=p.next;
+        }
+        return p.data;
+    }
+
+    public void set(int i, T x){
+        DoubleNode<T> p=this.head;
+        for (int k=0; p.next!=this.head && k<=i; k++){
+            p=p.next;
+        }
+        p.data=x;
+    }
+
+    @Override
+    public String toString(){
+        String string=this.getClass().getName()+"（";
+        DoubleNode<T> p=this.head.next;
+        while (p.next!=this.head){
+            string+=p.data+"，";
+            p=p.next;
+        }
+        string+=p.data.toString()+"）";
+        return string;
     }
 
     /**插入x为第i个元素，x!=null，返回x结点。对i容错，若i<0，则头插入;若i>长度n，则尾插入*/
@@ -62,6 +109,91 @@ public class CirDoublyList<T> {
         this.head.prev.next=p;
         this.head.prev=p;
         return p;
+    }
+
+    public T remove(int i){
+        DoubleNode<T> p=this.head.next;
+        for (int k=0; k<i && p!=this.head; k++){
+            p=p.next;
+        }
+        p.prev.next=p.next;
+        p.next.prev=p.prev;
+        return p.data;
+    }
+
+    public void clear(){
+        this.head.next=this.head;
+    }
+
+    /**查找，比相等*/
+    public DoubleNode<T> search(T key){
+        if (key==null){
+            throw new NullPointerException("key==null");
+        }
+        DoubleNode<T> p=this.head;
+        while (p.next!=this.head){
+            p=p.next;
+            if (key.equals(p.data)){
+                return p;
+            }
+        }
+        return null;
+    }
+
+    /**尾插入*/
+    public DoubleNode<T> insertDifferent(T x){
+        if (x==null){
+            throw new NullPointerException("x==null");
+        }
+        DoubleNode<T> p=this.head;
+        while (p.next!=this.head){
+            p=p.next;
+            if (x.equals(p.data)){
+                return null;
+            }
+        }
+        p.next=new DoubleNode<T>(x,p,this.head);
+        p.next.prev=p;
+        this.head.prev=p.next;
+        return p.next;
+    }
+
+    /**删除，重载
+     * 调用查找，比相等，运行时多态*/
+    public T remove(T key){
+        if (key==null){
+            throw new NullPointerException("key==null");
+        }
+        DoubleNode<T> p=this.search(key);
+        if (p!=null){
+            p.prev.next=p.next;
+            p.next.prev=p.prev;
+            return p.data;
+        }
+        return null;
+    }
+
+    @Override
+    public boolean equals(Object obj){
+        CirDoublyList<T> ObjCirDoublyList=(CirDoublyList<T>)obj;
+        DoubleNode<T> objNode=((CirDoublyList<T>) obj).head.next;
+        DoubleNode selfNode=this.head.next;
+        while ((objNode!=((CirDoublyList<T>) obj).head || selfNode!=this.head) && objNode.data.equals(selfNode.data)){
+            objNode=objNode.next;
+            selfNode=selfNode.next;
+        }
+        return (objNode==((CirDoublyList<T>) obj).head && selfNode==this.head);
+    }
+
+    /**合并连接*/
+    public void addAll(CirDoublyList<T> list){
+        DoubleNode<T> self=this.head.next;
+        while (self!=null){
+            self=self.next;
+        }
+        self.next=list.head;
+        list.head.prev=self;
+
     }
 
     /**返回所有元素的描述字符串，元素次序从后往前*/
--- tests/CirDoublyListTest.java
+++ tests/CirDoublyListTest.java
@@ -1,60 +1,123 @@
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.omg.PortableInterceptor.INACTIVE;
 
 import static org.junit.Assert.*;
 
 public class CirDoublyListTest {
     CirDoublyList<Integer> cirDoublyList;
-    CirDoublyList<Integer> cirDoublyList2;
     Integer[] integers={10,20,30};
-    Integer[] integers2={40,50,60};
 
     @Before
     public void setUp() throws Exception {
-        cirDoublyList=new CirDoublyList();
+        cirDoublyList=new CirDoublyList(integers);
     }
 
     @Test
     public void testCirDoublyList(){
-        cirDoublyList2=new CirDoublyList<Integer>(integers2);
+        Integer[] integers2={40,50,60};
+        CirDoublyList<Integer> cirDoublyList2=new CirDoublyList<Integer>(integers2);
         cirDoublyList=new CirDoublyList<>(cirDoublyList2);
-        System.out.print(cirDoublyList.toPreviousString());
+        System.out.print(cirDoublyList.toString());
     }
 
     @Test
     public void isEmpty() {
-        assertTrue(cirDoublyList.isEmpty());
+        assertFalse(cirDoublyList.isEmpty());
     }
 
     @Test
-    public void insert() {
+    public void testInsert() {
         cirDoublyList.insert(0,10);
-        System.out.print(cirDoublyList.toPreviousString());
+        System.out.print(cirDoublyList.toString());
     }
 
     @Test
-    public void insert1() {
+    public void testInsert1() {
         cirDoublyList.insert(10);
         cirDoublyList.insert(20);
-        System.out.print(cirDoublyList.toPreviousString());
+        System.out.print(cirDoublyList.toString());
     }
 
     @Ignore
     @Test
-    public void toPreviousString() {
+    public void testToPreviousString() {
         cirDoublyList.insert(10);
         cirDoublyList.insert(20);
         System.out.print(cirDoublyList.toPreviousString());
     }
 
     @Test
-    public void removeLast() {
+    public void testRemoveLast() {
         cirDoublyList.insert(10);
         cirDoublyList.insert(20);
         cirDoublyList.insert(1,50);
-        System.out.println("删除前："+cirDoublyList.toPreviousString());
+        System.out.println("删除前："+cirDoublyList.toString());
         assertEquals(20,(Object)cirDoublyList.removeLast());
-        System.out.println("删除后："+cirDoublyList.toPreviousString());
+        System.out.println("删除后："+cirDoublyList.toString());
     }
+
+    @Test
+    public void testSize(){
+        assertEquals(3,cirDoublyList.size());
+    }
+
+    @Test
+    public void teatGet(){
+        assertEquals(20,(Object)cirDoublyList.get(1));
+    }
+
+    @Test
+    public void testSet(){
+        cirDoublyList.set(0,11);
+        assertEquals(11,(Object)cirDoublyList.get(0));
+    }
+
+    @Test
+    public void testRemove(){
+        assertEquals(10,(Object)cirDoublyList.remove(0));
+        assertEquals(30,(Object)cirDoublyList.remove(1));
+        System.out.print(cirDoublyList.toString());
+    }
+
+    @Test
+    public void testClear(){
+        cirDoublyList.clear();
+        assertEquals(0,cirDoublyList.size());
+    }
+
+    @Test
+    public void testSearch(){
+        assertEquals(10,(Object)cirDoublyList.search(10).data);
+        assertEquals(30,(Object)cirDoublyList.search(30).data);
+        assertEquals(null,cirDoublyList.search(90));
+    }
+
+    @Test
+    public void testInsertDifferent(){
+        assertEquals(40,(Object)cirDoublyList.insertDifferent(40).data);
+        assertEquals(null,cirDoublyList.insertDifferent(10));
+        System.out.print(cirDoublyList.toString());
+    }
+
+    @Test
+    public void testRemove2(){
+        assertEquals(10,(Object)cirDoublyList.remove((Integer) 10));
+        assertEquals(30,(Object)cirDoublyList.remove((Integer) 30));
+        assertEquals(null,cirDoublyList.remove((Integer) 90));
+        System.out.print(cirDoublyList.toString());
+    }
+
+    @Test
+    public  void testEquals(){
+        Integer[] integers2={40,50,60};
+        Integer[] integers3={10,20,30};
+        CirDoublyList <Integer> cirDoublyList2=new CirDoublyList<Integer>(integers2);
+        CirDoublyList <Integer> cirDoublyList3=new CirDoublyList<Integer>(integers3);
+        assertTrue(cirDoublyList.equals(cirDoublyList3));
+        assertFalse(cirDoublyList.equals(cirDoublyList2));
+    }
+
+
 }
